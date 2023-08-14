5a78d4240f1586d79f214829ea172ecc681bc33a
sonya7043 <sonya7043@naver.com>
2023-08-14 11:10:00 +0900

    수정

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,5 +1,13 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
 
+	@GetMapping("/member")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
